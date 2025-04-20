91ed467d3cc94daeb992c14a8fe1f518e20faf4d
An Nguyen <21521810@gm.uit.edu.vn>
2025-04-20 12:41:53 +0700

    fix sign_bit in ALU

--- hw_design/coherence_cache/coherence_cache.srcs/sources_1/new/alu.v
+++ hw_design/coherence_cache/coherence_cache.srcs/sources_1/new/alu.v
@@ -11,23 +11,26 @@ module alu
     input  [31:0]     i_alusrcA,
     input  [31:0]     i_alusrcB,
     output reg [31:0] o_alu_result,
-    output reg        o_sign_bit
+    output            o_sign_bit
 );
 
     always @(*) begin
         casex (i_alu_control)
-            `ALU_ADD : {o_sign_bit, o_alu_result} = i_alusrcA + i_alusrcB;
-            `ALU_SUB : {o_sign_bit, o_alu_result} = i_alusrcA - i_alusrcB;
-            `ALU_SLL : {o_sign_bit, o_alu_result} = i_alusrcA << i_alusrcB[4:0];
-            `ALU_SSUB: {o_sign_bit, o_alu_result} = $signed(i_alusrcA) - $signed(i_alusrcB);
-            `ALU_USUB: {o_sign_bit, o_alu_result} = $unsigned(i_alusrcA) - $unsigned(i_alusrcB);
-            `ALU_XOR : {o_sign_bit, o_alu_result} = i_alusrcA ^ i_alusrcB;
-            `ALU_SRL : {o_sign_bit, o_alu_result} = $unsigned(i_alusrcA) >> $unsigned(i_alusrcB[4:0]);
-            `ALU_SRA : {o_sign_bit, o_alu_result} = $signed(i_alusrcA) >> $signed(i_alusrcB[4:0]);
-            `ALU_OR  : {o_sign_bit, o_alu_result} = i_alusrcA | i_alusrcB;
-            `ALU_AND : {o_sign_bit, o_alu_result} = i_alusrcA & i_alusrcB;
-            default  : {o_sign_bit, o_alu_result} = 0;
+            `ALU_ADD : o_alu_result = i_alusrcA + i_alusrcB;
+            `ALU_SUB : o_alu_result = i_alusrcA - i_alusrcB;
+            `ALU_SLL : o_alu_result = i_alusrcA << i_alusrcB[4:0];
+            `ALU_SSUB: o_alu_result = $signed(i_alusrcA) - $signed(i_alusrcB);
+            `ALU_USUB: o_alu_result = $unsigned(i_alusrcA) - $unsigned(i_alusrcB);
+            `ALU_XOR : o_alu_result = i_alusrcA ^ i_alusrcB;
+            `ALU_SRL : o_alu_result = i_alusrcA >> i_alusrcB[4:0];
+            `ALU_SRA : o_alu_result = $signed(i_alusrcA) >>> i_alusrcB[4:0];
+            `ALU_OR  : o_alu_result = i_alusrcA | i_alusrcB;
+            `ALU_AND : o_alu_result = i_alusrcA & i_alusrcB;
+            default  : o_alu_result = 0;
         endcase
     end
     
+    assign o_sign_bit = (i_alu_control == `ALU_SSUB)  ? ($signed(i_alusrcA) < $signed(i_alusrcB)) :
+                        (i_alu_control == `ALU_USUB)  ? ($unsigned(i_alusrcA) < $unsigned(i_alusrcB)) : 1'b0;
+
 endmodule
