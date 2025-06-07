1de40e8eea38031c73b0886cdbc88dbda79a676f
An Nguyen <21521810@gm.uit.edu.vn>
2025-06-07 11:49:34 +0700

    update to create result files

--- hw_design/testcase/assembly/script.py
+++ hw_design/testcase/assembly/script.py
@@ -7,9 +7,10 @@ output_dir = ""
 for k in range(1, 21):
     filename_0 = f"testcase_A_{k}.asm"
     filename_1 = f"testcase_B_{k}.asm"
+    filename_2 = f"result_A_{k}.txt"
+    filename_3 = f"result_B_{k}.txt"
 
     filepath = os.path.join(output_dir, filename_0)
-
     with open(filepath, "w") as f:
         f.write(f"# This is {filename_0}\n")
         f.write("# Description: \n\n")
@@ -18,12 +19,21 @@ for k in range(1, 21):
     f.close()
 
     filepath = os.path.join(output_dir, filename_1)
-
     with open(filepath, "w") as f:
         f.write(f"# This is {filename_1}\n")
         f.write("# Description: \n\n")
         f.write(".text\n")
         f.write("# assembly code is start here!\n")
+    f.close()
+
+    filepath = os.path.join(output_dir, filename_2)
+    with open(filepath, "w") as f:
+        f.write(f"# Result of testcase_A_{k}!\n")
+    f.close()
+
+    filepath = os.path.join(output_dir, filename_3)
+    with open(filepath, "w") as f:
+        f.write(f"# Result of testcase_B_{k}!\n")
     f.close()
 
 print(f"20 .asm files created in '{output_dir}' folder.")
